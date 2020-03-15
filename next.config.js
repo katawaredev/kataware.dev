3947a129baf517f16497e2075a4980d9ec6b57bd
Nikolay Stoynov <arvigeus@gmail.com>
2020-03-15 20:34:47 +0200

    feat: enable absolute imports

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
+const path = require("path")
+
 const withImages = require('next-images')
 module.exports = withImages({
   webpack(config, options) {
+    config.resolve.alias['components'] = path.join(__dirname, "src", 'components')
     return config
   }
 })
